7d47d225902b64bd8933bcfe868d209a9e6d4384
goby <goby@foxmail.com>
2014-10-21 20:20:15 +0800

    delete

--- src/include/defs.h
+++
@@ -1,6 +0,0 @@
-#ifndef __WEMCACHED_INCLUDE_DEFS_H__
-#define __WEMCACHED_INCLUDE_DEFS_H__
-#undef  VERSION
-#define VERSION "0.0.1"
-
-#endif
--- src/include/server.h
+++
@@ -1,6 +0,0 @@
-#ifndef __WEMCACHED_CONFIG_H__
-#define __WEMCACHED_CONFIG_H__
-
- int serv();
-
-#endif
